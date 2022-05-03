908ac6a63086a0b2e1057207c8acabc4ea1af74e
leptyukh <lnv.danil@bk.ru>
2022-05-03 18:50:31 +0300

    6 от 02.05
    наконец то exe

---
+++ exe/main.py
@@ -0,0 +1,21 @@
+import eel 
+import os
+path_init_file = os.path.abspath('page')
+print(path_init_file)
+@eel.expose
+def snake():
+    os.startfile('snake.py')
+
+@eel.expose
+def tetris():
+    os.startfile('tetris.py')
+@eel.expose
+def zero():
+    os.startfile('zero.py')
+eel.init(path_init_file) 
+
+@eel.expose
+def sea():
+    os.startfile('sea.py')   
+eel.start('launch.html', size=(1920, 1080))
+
